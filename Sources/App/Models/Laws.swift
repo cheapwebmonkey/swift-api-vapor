0fd2163b1a3c9b23418ce4e06cf782f4b52c0685
Margeaux Spring <contact@cheapwebmonkey.com>
2017-03-30 16:36:53 -0400

    inital commit

---
+++ Sources/App/Models/Laws.swift
@@ -0,0 +1,54 @@
+//
+//  Law.swift
+//  Laws
+//
+//  Created by Margeaux Spring on 3/30/17.
+//
+//
+
+import Foundation
+import Vapor
+
+struct Law: Model {
+    var exists: Bool = false
+    var id: Node?
+    let legislator: String
+    let bill: String
+    let vote: String
+    
+    init(legislator: String, bill: String, vote: String) {
+        self.legislator = legislator
+        self.bill = bill
+        self.vote = vote
+    }
+    
+    // NodeInitializable
+    init(node: Node, in context: Context) throws {
+        id = try node.extract("id")
+        legislator = try node.extract("legislator")
+        bill = try node.extract("bill")
+        vote = try node.extract("vote")
+    }
+    
+    // NodeRepresentable
+    func makeNode(context: Context) throws -> Node {
+        return try Node(node: ["id": id,
+                               "legislator": legislator,
+                               "bill": bill,
+                               "vote": vote])
+    }
+    
+    // Preparation
+    static func prepare(_ database: Database) throws {
+        try database.create("laws") { laws in
+            laws.id()
+            laws.string("legislator")
+            laws.string("bill")
+            laws.string("vote")
+        }
+    }
+    
+    static func revert(_ database: Database) throws {
+        try database.delete("laws")
+    }
+}
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -8,6 +8,28 @@ drop.get { req in
     ])
 }
 
+//drop.get("laws") { req in
+//    return try JSON(node: ["laws": [
+//        ["legislator": "Sarah Thomas", "bill": "SB2", "vote": "y"],
+//        ["legislator": "Steve Burbank", "bill": "SB2", "vote": "y"],
+//        ["legislator": "Drew Hardy", "bill": "SB2", "vote": "n"]
+//        ]
+//    ])
+//}
+
+drop.get("laws") { req in
+    let laws = [
+        Law(legislator: "Sarah Hanson", bill: "SB2", vote:"y"),
+        
+        Law(legislator: "Steve Sherman", bill: "SB2", vote:"y"),
+        
+        Law(legislator: "Drew Hardy", bill: "SB2", vote:"n")
+    ]
+    let lawsNode = try laws.makeNode()
+    let nodeDictionary = ["laws": lawsNode]
+    return try JSON(node: nodeDictionary)
+}
+
 drop.resource("posts", PostController())
 
 drop.run()
